b9cd4216156b511494cd2aef981d8bf56d3edd4a
Joyswedo <tawoderoeme.jn@gmail.com>
2026-08-12 03:19:32 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="sidderaal-v1";
-const ASSETS=["./","./index.html","./admin.html","./style.css","./app.js","./manifest.json","./icons/icon-192.png","./icons/icon-512.png"];
+const CACHE="sidderaal-v2-logo";
+const ASSETS=["./","./index.html","./admin.html","./style.css","./app.js","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
